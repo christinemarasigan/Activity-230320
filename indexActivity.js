4c099e090ddb346f0d19dfd146fc3b7fc0dd36cb
Christine Marasigan <mct1174@dlsud.edu.ph>
2023-03-20 23:59:48 +0800

    Add activity code

---
+++ indexActivity.js
@@ -0,0 +1,28 @@
+console.log('Hello, Welcome to De La Salle University!');
+
+let firstName = 'Christine';
+let lastName = 'Marasigan';
+let age = 20;
+
+let hobbies = [
+    'Read fictional books', 'Playing video games', 'Watch fantasy movies', 'Listen to music', 
+    'Play badminton'
+];
+
+let homeAddress = {
+    houseNumber: 'Block 18 Lot 22',
+    street: 'Riviera Street',
+    village: 'Fairway View Village',
+    barangay: 'Salawag',
+    city: 'Dasmarinas City',
+    province: 'Cavite',
+    zipCode: 4114
+};
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hobbies);
+console.log(homeAddress);
+
+
